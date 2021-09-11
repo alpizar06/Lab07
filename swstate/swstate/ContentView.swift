3dce7846d5fdfbf83e811762defc47fdcc373a7b
Daniel Alpizar <alpizar0610@gmail.com>
2021-09-11 11:37:04 -0600

    Add files via upload

---
+++ swstate/swstate/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  swstate
+//
+//  Created by Daniel Alpizar on 11/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   @State var pokemonName = "---"
+    var body: some View {
+        VStack{
+            Text("Random Pokemon Generator")
+            Text(pokemonName)
+                .frame(
+                    width: UIScreen.main.bounds.width, height: 50
+                )
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .padding(10)
+            Button(action: { self.switchPokemon() }, label: { Text("Run")}
+            )
+        }
+    }
+    func switchPokemon(){
+        let list = ["Squirtle","Bulbasaur","Charmander","Pikachu"]
+        pokemonName = list.randomElement() ?? ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
